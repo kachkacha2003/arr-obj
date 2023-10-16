6e8a14059ce1d2230e382353895312b4dade44e1
kachkacha2003 <lasha.katchkatchishvili.1@btu.edu.ge>
2023-10-16 21:05:56 +0400

    tasks

---
+++ script.js
@@ -0,0 +1,67 @@
+// let a="tansacmeli";
+// let b="fexsacmeli";
+// let contract="furceli";
+// let func=(item)=>{
+//     console.log( "I am playing " + item);
+// }
+
+// let person={
+//     name:"nika",
+//     age:20,
+//     education:"bitcamp",
+//     gender:"male",
+//     numbers:[1,2,3,4],
+//     hobby:func
+// }
+// delete person.numbers;
+// person.numbers=[1,2,3];
+// // console.log(person)
+
+// let arr=[1,2,3,true,"bitcamp",null,false,person];
+
+
+// console.log(arr[arr.length-1])
+
+// 1)დაწერეთ პროგრამა, რომ იპოვოთ მასივის ყველა რიცხვის ჯამი.
+
+// let array=[1,2,3,4,5,6,7,8,9,10]
+// function sum(arr){
+//     let count=0;
+//     for(let i=0;i<arr.length;i++){
+//       count+=arr[i];
+//     }
+//     return count;
+// }
+// console.log(sum(array))
+
+// 2)დაწერეთ პროგრამა მასივში რიცხვების საშუალოს საპოვნელად.
+
+
+// let array=[1,2,3,4,5,6,7,8,9,10]
+// function sum(arr){
+//     let count=0;
+//     for(let i=0;i<arr.length;i++){
+//       count+=arr[i];
+//     }
+//     return count/arr.length;
+// }
+// console.log(sum(array))
+
+
+// 3)დაწერეთ პროგრამა რიცხვების მასივის ზრდის მიხედვით დასალაგებლად.
+
+// let arr=["d","c","b","a"];
+
+// let sorted=arr.sort();
+
+// console.log(sorted)
+// let arr=[5,4,8,13,10,19,16];
+// let max=Math.max(...arr);
+// console.log(max)
+// function sortArray(arr){
+//   let sorted=arr.sort((a,b)=>{
+//         return b-a
+//     });
+//     return sorted[0]
+// }
+// console.log(sortArray(arr));
